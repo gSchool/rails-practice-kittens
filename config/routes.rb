677e4ab7bb1d34ed3027c62f59cefdc9b2c082f6
Jeff Dean <jeff@galvanize.it>
2014-07-20 13:56:23 -0600

    users can view categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "signin" => "sessions#new", as: :signin
   post "signin" => "sessions#create"
   get "signout" => "sessions#destroy", as: :signout
+  get "/categories" => "categories#index", as: :categories
 end
